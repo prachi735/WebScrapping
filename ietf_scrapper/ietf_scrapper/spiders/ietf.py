67ecbeef16e94818b2db397d68cdbe39f8b1e3a8
Prachi Singh <prachi735@users.noreply.github.com>
2022-05-18 10:45:45 +0000

    Initial Commit

---
+++ ietf_scrapper/ietf_scrapper/spiders/ietf.py
@@ -0,0 +1,15 @@
+from matplotlib.pyplot import title
+import scrapy
+
+
+class IetfSpider(scrapy.Spider):
+    name = 'ietf'
+    allowed_domains = ['pythonscrapping.com']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
+
+    def parse(self, response):
+        # title = response.css('span.title::text').get()
+        title = response.xpath('//span[@class="title]/text()').get()
+        return {"title":title}
+        
+
